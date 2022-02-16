3a952937af288655758aa429ee21f2c9c4bb10f8
John-McPherson <john-mac1989@hotmail.co.uk>
2022-02-16 12:14:02 +0000

    add test suits to todo dirct

---
+++ todo/test_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+
+class testDjango(TestCase):
+
+    def test_this_thing_works(self):
+        self.assertEqual(1, 1)
+    
+    def test_this_thing_works2(self):
+        self.assertEqual(2, 2)
+
+    def test_this_thing_works3(self):
+        self.assertEqual(1, 1)
+    
+    def test_this_thing_works4(self):
+        self.assertEqual(1, 1)
+
+
+
